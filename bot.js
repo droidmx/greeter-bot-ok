c1e0af255048a10a48d683face780d68b19260db
derivedtv <38233444+derivedtv@users.noreply.github.com>
2018-05-12 13:38:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,62 @@ const prefix = '!';
 const ownerid =  "160140367554019329"
 const music = require('discord.js-musicbot-addon');
 const snekfetch = require("snekfetch");
+const setupCMD = "!createrolemessage"
+
+let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
+const roles = ["Fortnite", "Rocket League", "Overwatch", "RotMG", "GTAV"];
+const reactions = ["444914030709178378", "444914415133917184", "444914414433337355", "444914412395036685", "444914415100231690"];
+if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+
+function generateMessages(){
+  var messages = [];
+  messages.push(initialMessage);
+  for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); //DONT CHANGE THIS
+  return messages;
+}
+
+client.on("message", message => {
+  if (message.author.id == "160140367554019329" && message.content.toLowerCase() == setupCMD){
+      var toSend = generateMessages();
+      let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+      for (let mapObj of mappedArray){
+          message.channel.send(mapObj[0]).then( sent => {
+              if (mapObj[1]){
+                sent.react(mapObj[1]);  
+              } 
+          });
+      }
+  }
+})
+
+
+client.on('raw', event => {
+  if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+      
+      let channel = client.channels.get(event.d.channel_id);
+      let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+      let user = msg.guild.members.get(event.d.user_id);
+      
+      if (msg.author.id == client.user.id && msg.content != initialMessage){
+     
+          var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+          var role = msg.content.match(re)[1];
+      
+          if (user.id != client.user.id){
+              var roleObj = msg.guild.roles.find('name', role);
+              var memberObj = msg.guild.members.get(user.id);
+              
+              if (event.t === "MESSAGE_REACTION_ADD"){
+                  memberObj.addRole(roleObj)
+              } else {
+                  memberObj.removeRole(roleObj);
+              }
+          }
+      }
+      })
+
+  }   
+});
 
 music(client, {
   prefix: "!",
